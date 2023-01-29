decb023a1414979db4997eca14d09e9d5f98b19b
Asep Ardi <ardi.winardi91@gmail.com>
2023-01-29 17:39:23 +0700

    perform: enable pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  publicExcludes: ['!assets/**/*'],
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   output: 'standalone',
-};
-
-module.exports = nextConfig;
+});
